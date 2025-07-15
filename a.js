1437fece9ec3335819c88e9ca42273d62ce91ddb
sindhu292006 <bhavyasindhuhapedapati@gmail.com>
2025-07-15 10:56:09 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,188 @@
+/*alert("welcome to IT-B");
+console.log("hello world");
+console.log('welcome to mrec');
+console.log('hello world...')
+var a=20;
+var a=30;
+var a=60;
+console.log(a);
+
+let b=10;
+b=30;
+console.log(b);
+
+const pi=3.14;
+console.log(pi)
+
+let a=20;
+let A=20;
+console.log(a);
+console.log(A);*/
+/*
+let _a=50;
+console.log()
+let user_name="sindhu";
+console.log(user_name);*/
+
+let $p=20;
+console.log("$p=",$p);
+
+let user_name="sindhu";
+console.log(user_name)
+
+/* 
+datatypes:
+2 Types
+premitive(im-mutable) and non-premitive
+Number
+String
+boolean
+null
+undefined
+Symbol
+BigInt
+//
+var a=10;
+console.log(typeof(a));
+let name="charana";
+console.log(typeof(name));
+let b=true;
+console.log(typeof(b));
+let c=null;
+console.log(typeof(c));
+var a=12;
+console.log(typeof(a));
+let cgpa=9.87;
+console.log(cgpa);
+console.log(typeof(cpga));
+let k=null;
+console.log(typeof(k));
+let num=123445n;
+console.log(typeof(num));
+isfollow=true;
+r=typeof(isfollow);
+console.log(r);*/
+/*
+const p={
+    name:"charana",
+    age:19,
+    location:"hyd";
+}
+
+console.log(p);
+console.log(p.age);
+console.log(p.location);
+//
+const studentdetails={
+    name:"charana",
+    age:19,
+    year:"3rd year",
+    department:"information technology",
+    collage:"mrec",
+    rollno:"23J41A12G0",
+    
+}
+console.log(studentdetails);
+console.log("age =",studentdetails.name,"year =",studentdetails.year);
+console.log(studentdetails.age);
+console.log(studentdetails.year);
+console.log(studentdetails.department);
+console.log(studentdetails.collage);
+console.log(studentdetails.rollno);//
+alert("instagram login page");
+const instagramprofile={
+    userid:"sricharana_04",
+    bio:"keep going on",
+    posts:7,
+    followers:10000,
+    following:5000,
+    isfollow:true,
+}
+
+console.log(instagramprofile);
+console.log(instagramprofile.userid);
+console.log(instagramprofile.bio);
+console.log(instagramprofile.posts);
+console.log(instagramprofile.followers);
+console.log(instagramprofile.following);
+console.log(instagramprofile.isfollow);
+
+console.log(typeof(instagramprofile));
+console.log(typeof(instagramprofile.userid));
+console.log(typeof(instagramprofile.bio));
+console.log(typeof(instagramprofile.posts));
+console.log(typeof(instagramprofile.followers));
+console.log(typeof(instagramprofile.following));
+console.log(typeof(instagramprofile.isfollow));
+*//*string
+let fruits=["apple","banana","cherry","mango"]
+console.log(fruits);
+console.log(fruits[1]);*/
+
+/*function a(name){
+    console.log("hello"+name);
+}
+a("sindhu");
+function a(name){
+    console.log("hello"+name);
+}a("akhil");*/
+// operators
+// arthematic operators
+// +Addition 
+// -subraction
+// *multicplication
+// /divison
+// %modulus
+// **exponent
+// let a=5;
+// let b=2;
+// console.log("a+b= ",a+b);
+// console.log("a-b= ",a-b);
+// console.log("a*b= ",a*b);
+// console.log("a/b= ",a/b);
+// console.log("a%b= ",a%b);
+// console.log("a**b= ",a**b);
+
+//  let a=5;
+// let b=2;
+// console.log(++a);
+// console.log(b++);
+// console.log(++a);
+// let a=5;
+//  let b=2;
+//  console.log(--a);
+//  console.log(b--);
+//  console.log(a--);
+//  Assignment operators
+// = a=10;
+// += a+=10;
+// -= a-=10;
+// *= a*=10;
+// **= a**=10;
+// let a=5;
+// consolea=5;
+// console.log(a+=10);
+// console.log(a-=2);
+// let b=10;
+// console.log(a*=2);
+// console.log(b%=2);
+// let c=6;
+// console.log(c**=3);
+// comparision operator
+// ==
+// !=
+// >=
+// <=
+// <
+// >
+// ===(strict equals
+    // !==(strictnot equals)
+
+let a=10;
+let b="10",c=5;
+console.log("a==b: ",a==b);
+console.log("a===b: ",a===b);
+console.log("a!=b: ",a!=b);
+console.log("a!==b: ",a!==b);
+
+
